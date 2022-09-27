8467bacdeb61a3c45db9a076f7e2a0efb5898930
bilal karahan <bilalkarahan309@gmail.com>
2022-09-27 10:28:46 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
+    static boolean isPalindrom(int number){
+        int temp=number,reverseNumber=0,lastNumber;
+        while (temp!=0){
+            lastNumber=temp%10;
+            reverseNumber=(10*reverseNumber)+lastNumber;
+            temp=temp/10;
+        }
+                if(reverseNumber==number)
+                    return true;
+                else return false;
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrom(101));
     }
 }
